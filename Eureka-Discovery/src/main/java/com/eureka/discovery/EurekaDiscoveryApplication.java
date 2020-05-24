2de71ad884ff908bac09b8bda58c89b066012354
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 01:20:46 +0530

    @EnableEurekaServer

--- Eureka-Discovery/src/main/java/com/eureka/discovery/EurekaDiscoveryApplication.java
+++ Eureka-Discovery/src/main/java/com/eureka/discovery/EurekaDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.eureka.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaDiscoveryApplication {
 
 	public static void main(String[] args) {
