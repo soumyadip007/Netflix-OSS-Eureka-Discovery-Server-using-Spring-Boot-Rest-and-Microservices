85519e0f1258b4deef41b66a141c3752a52b3ade
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 15:11:12 +0530

    Rating res

---
+++ Eureka-Discovery/Client-Service-2/ratingsdataservice/resources/RatingsResource.java
@@ -0,0 +1,27 @@
+package com.eureka.ratingsdataservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eureka.ratingsdataservice.model.Rating;
+import com.eureka.ratingsdataservice.model.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
+}
